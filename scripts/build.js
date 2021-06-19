8f70d0c37e342a53365560149bc7fcff06317c0a
Eugene Obrezkov <ghaiklor@gmail.com>
2021-06-19 13:44:35 +0300

    Improve build script with detection of modified sources

--- scripts/build.js
+++ scripts/build.js
@@ -4,14 +4,17 @@ const child_process = require("child_process");
 const util = require("util");
 const sources = fs.readdirSync(path.resolve(__dirname, "..", "src"));
 
-(async function () {
-  await Promise.all(
-    sources.map(async (source) => {
-      const absolutePath = path.resolve(__dirname, "..", "src", source);
-      const slideName = source.split(".")[0];
+(async function main() {
+  for (const source of sources) {
+    const absolutePath = path.resolve(__dirname, "..", "src", source);
+    const slideName = source.split(".")[0];
 
-      await util.promisify(child_process.exec)(`slidev format ${absolutePath}`);
-      await util.promisify(child_process.exec)(`slidev build --out ${slideName} --base /${slideName}/ ${absolutePath}`);
-    })
-  );
+    if (fs.existsSync(slideName) && fs.statSync(absolutePath).mtime === fs.statSync(slideName).mtime) {
+      console.info(`Skipping the build for ${slideName}, it's already built`);
+      continue;
+    }
+
+    console.info(`Building ${slideName}...`);
+    await util.promisify(child_process.exec)(`slidev build --out ${slideName} --base /${slideName}/ ${absolutePath}`);
+  }
 })();
