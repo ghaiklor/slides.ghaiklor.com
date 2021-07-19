e3bf6aefec0f4d01e23e8291ec56e9379cbaf61b
Eugene Obrezkov <ghaiklor@gmail.com>
2021-07-19 14:16:33 +0300

    [chore]: format markdown files sequentially

--- scripts/format.js
+++ scripts/format.js
@@ -4,12 +4,11 @@ const child_process = require("child_process");
 const util = require("util");
 const sources = fs.readdirSync(path.resolve(__dirname, "..", "src"));
 
-(async function main() {
-  await Promise.all(
-    sources.map(async (source) => {
-      const absolutePath = path.resolve(__dirname, "..", "src", source);
-      console.info(`Formatting ${source}...`);
-      await util.promisify(child_process.exec)(`slidev format ${absolutePath}`);
-    })
-  );
+(async function format() {
+  for await (const source of sources) {
+    const absolutePath = path.resolve(__dirname, "..", "src", source);
+
+    console.info(`Formatting ${source}...`);
+    await util.promisify(child_process.exec)(`slidev format ${absolutePath}`);
+  }
 })();
