670ef856a7dee72330e526d5608778a024d9d4a7
Eugene Obrezkov <ghaiklor@gmail.com>
2021-08-16 19:04:06 +0300

    [chore]: improve build script to specify specific slides

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,20 @@ const path = require("path");
 const child_process = require("child_process");
 const util = require("util");
 const sources = fs.readdirSync(path.resolve(__dirname, "..", "src"));
+const buildOnly = process.argv.slice(2).map((slide) => path.resolve(__dirname, "..", slide));
 
 (async function build() {
   for await (const source of sources) {
     const absolutePath = path.resolve(__dirname, "..", "src", source);
     const slideName = source.split(".")[0];
 
-    console.info(`Building ${slideName}...`);
-    await util.promisify(child_process.exec)(`slidev build --out ${slideName} --base /${slideName}/ ${absolutePath}`);
+    if (buildOnly.includes(absolutePath) || buildOnly.length === 0) {
+      console.info(`Building ${slideName}...`);
+      await util.promisify(child_process.exec)(
+        `./node_modules/.bin/slidev build --out ${slideName} --base /${slideName}/ ${absolutePath}`
+      );
+    } else {
+      console.info(`Skipping ${slideName}...`);
+    }
   }
 })();
